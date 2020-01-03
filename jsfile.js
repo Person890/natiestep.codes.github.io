562079160771ccb7b8adc89b7e895a951b8c79fe
Person890 <44305879+Person890@users.noreply.github.com>
2020-01-02 19:13:30 -0800

    commit

--- jsfile.js
+++ jsfile.js
@@ -2,3 +2,15 @@ $(document).ready(function() {
     jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000  // 2 seconds
   });
 
+  
+  (function($) {          
+      $(document).ready(function(){                    
+          $(window).scroll(function(){                          
+              if ($(this).scrollTop() > 200) {
+                  $('#menu').fadeIn(500);
+              } else {
+                  $('#menu').fadeOut(500);
+              }
+          });
+      });
+  })(jQuery);
