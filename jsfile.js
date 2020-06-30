569575d44556fa5f9d0f8587de19a2a752cc4d4b
Person890 <44305879+Person890@users.noreply.github.com>
2020-06-29 19:22:21 -0700

    commit

--- jsfile.js
+++ jsfile.js
@@ -17,3 +17,14 @@ $(document).ready(function() {
           });
       });
   })(jQuery);
+
+
+  function switchStyle() {
+    if (document.getElementById('styleSwitch').checked) {
+      document.getElementById('gallery').classList.add("custom");
+      document.getElementById('exampleModal').classList.add("custom");
+    } else {
+      document.getElementById('gallery').classList.remove("custom");
+      document.getElementById('exampleModal').classList.remove("custom");
+    }
+  }
