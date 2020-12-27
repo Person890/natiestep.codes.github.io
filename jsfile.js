343c66dfa3fc77e6b038a7c594c5e100234b86e2
Person890 <44305879+Person890@users.noreply.github.com>
2020-12-27 00:28:37 -0800

    commit

--- jsfile.js
+++ jsfile.js
@@ -1,6 +1,7 @@
  import Masonry from 'masonry-layout';
 
 
+
 $(document).ready(function() {
     jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 5000  // 2 seconds
   });
@@ -25,3 +26,4 @@ $(document).ready(function() {
     
   }
 
+ 
